25ed53b3dbe542f2d4b70679bc772aa7af16932f
Breanna Clary <bclary@liverpool.ac.uk>
2022-05-24 15:29:52 +0100

    Outline Initial Classes and Functions

---
+++ Checkout.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class Checkout
+    {
+        decimal _total = 0;
+        List<Item> _itemList = new List<Item>();
+        
+        public decimal Total()
+        {
+            return _total;
+        }
+
+        public void Scan(Item item)
+        {
+
+        }
+
+        public List<Item> ShowScannedItems()
+        {
+
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class Item
+    {
+        string _sku;
+        decimal _unitprice;
+        int _itemcount;
+
+        public string Sku { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int ItemCount { get; set; }
+
+        public decimal ItemtotalCost()
+        {
+
+        }
+
+    }
+}
